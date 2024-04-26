b092f838a3d22cacb15b7647c58e68fd3bd0e785
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-04-26 16:49:39 -0300

    delete about

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/homePage/HomePage"
-import AboutPage from "./pages/aboutPage/AboutPage"
-import ProjectsPage from "./pages/projectsPage/ProjectsPage"
+
 import SkillsPage from "./pages/skillsPage/SkillsPage"
-import BackgroundPage from "./pages/backgroundPage/BackgroundPage"
 import ContactPage from "./pages/contactPage/ContactPage"
-import TriarPage from "./pages/triarPage/TriarPage"
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/about' element={<AboutPage/>}/>
-        <Route path='/projects' element={<ProjectsPage/>}/>
+
         <Route path='/skills' element={<SkillsPage/>}/>
-        <Route path='/background' element={<BackgroundPage/>}/>
         <Route path='/contact' element={<ContactPage/>}/>
-        <Route path='/triar' element={<TriarPage/>}/>
       </Routes>
     </div>
   )
--- src/components/homeComp/HomeComp.tsx
+++ src/components/homeComp/HomeComp.tsx
@@ -1,9 +1,8 @@
 import styles from './_HomeComp.module.scss'
 import { useSelector } from 'react-redux';
 import { selectMoonState } from '../../redux/slices/moonSlice';
-import { selectLangState } from '../../redux/slices/langSlice';
+// import { selectLangState } from '../../redux/slices/langSlice';
 import SliderComp from '../sliderComp/SliderComp';
-import ProfileImg from '../../assets/images/profile/Profile.jpg'
 import LinksComp from '../linksComp/LinksComp';
 
 
@@ -13,7 +12,7 @@ const HomeComp = () => {
   
   const dataColor = `${styles.data} ${moonState.moon ? styles.dataWhite : ''}`;
 
-  const langState = useSelector(selectLangState).lang;
+  // const langState = useSelector(selectLangState).lang;
 
   return (
     <div className={styles.container}>
@@ -21,7 +20,7 @@ const HomeComp = () => {
         <div className={styles.dataContainer}>
           <div className={dataColor}>
             <h4 className={styles.hi}>
-            {langState === 'es' ? "Hola Soy" : "Hi I'm"}
+            {/* {langState === 'es' ? "Hola Soy" : "Hi I'm"} */}
             </h4>
             <h2
               className={styles.name}
@@ -31,26 +30,22 @@ const HomeComp = () => {
             <h5
               className={styles.role}
             >
-              {langState === 'es' ? 'Programador Web Full Stack' : 'Full Stack Web Developer'}
+              {/* {langState === 'es' ? 'Programador Web Full Stack' : 'Full Stack Web Developer'} */}
             </h5>
             <p
               className={styles.text}
             >
-              {langState === 'es' ? 
+              {/* {langState === 'es' ? 
                 'Este es mi portafolio web, el cual muestra mis proyectos, habilidades y formación académica.' 
-                : 'This is my web portfolio, which shows my projects, skills and academic training.'}
+                : 'This is my web portfolio, which shows my projects, skills and academic training.'} */}
             </p>
           <LinksComp/>
           </div>
-            <img
-              src={ProfileImg}
-              className={styles.profileImg}
-            />
       </div>
-      <div className={styles.shape}>
+      {/* <div className={styles.shape}>
         <h4 className={styles.firstName}>Jorge</h4>
         <p className={styles.lastName}>ACOSTA</p>
-      </div>
+      </div> */}
     </div>
   );
 }

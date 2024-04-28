6975efc4b74ae5b329df10966ad3b87373718993
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-04-28 17:57:45 -0300

    About y contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/homePage/HomePage"
-// import SkillsPage from "./pages/skillsPage/SkillsPage"
-// import ContactPage from "./pages/contactPage/ContactPage"
+import AboutPage from "./pages/aboutPage/AboutPage"
+import ContactPage from "./pages/contactPage/ContactPage"
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
-        {/* <Route path='/skills' element={<SkillsPage/>}/>
-        <Route path='/contact' element={<ContactPage/>}/> */}
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/contact" element={<ContactPage/>}/>
       </Routes>
     </div>
   )

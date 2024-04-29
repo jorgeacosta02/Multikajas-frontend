9661716cda956e7801c889a1037a3020129825ff
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-04-29 13:42:24 -0300

    protectedRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/homePage/HomePage"
 import ContactPage from "./pages/contactPage/ContactPage"
+import UserRoutesComp from "./components/protectedRoutes/userRoutesComp/UserRoutesComp"
 import IntranetPage from "./pages/intranetPage/IntranetPage"
 import ArticleUpFormPage from "./pages/articleUpFormPage/ArticleUpFormPage"
 
@@ -11,7 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path="/contact" element={<ContactPage/>}/>
-        <Route path="/intranet" element={<IntranetPage/>}/>
+        <Route element={<UserRoutesComp/>}>
+          <Route path='/intranet' element={<IntranetPage/>} />
+        </Route>
         <Route path="/article-up-form" element={<ArticleUpFormPage/>}/>
       </Routes>
     </div>
---
+++ src/components/protectedRoutes/userRoutesComp/UserRoutesComp.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { getDataFromLocalStorage } from "../../localStorageComp/LocalStorageComp";
+
+
+const UserRoutesComp = () => {
+
+  const accessLogin = getDataFromLocalStorage('accessLogin');
+
+  console.log(accessLogin)
+
+  // if ( accessLogin.user.active && accessLogin.user.role === 'user' ) {
+  //   console.log('dentro del if')
+  //   return <Outlet/>;
+  // }
+  console.log('fuera del if')
+  return <Navigate to='/login' />
+};
+
+export default UserRoutesComp
